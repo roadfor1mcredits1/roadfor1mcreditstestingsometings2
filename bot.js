068fd68c0e7b5c42ebcab883adfe24a531886197
roadfor1mcredits1 <44210726+roadfor1mcredits1@users.noreply.github.com>
2018-11-04 12:01:11 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', message => {
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "1say") {
+  if (command == "ay") {
    message.channel.sendMessage(args.join("  "))
    message.delete()
   }
@@ -23,7 +23,28 @@ client.on('message', message => {
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
 
+client.on('warn', console.warn);
 
+client.on('error', console.error);
+// Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 500);
 
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
